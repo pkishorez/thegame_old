20cc0c8b2bfc791bb6cc70997533e05574aea782
kishoreoyo <kishore.polamarasetty@oyo.com>
2020-07-12 23:34:36 +0530

    Update docs with .nojekyll

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3449fc7c5d426a462924";
+/******/ 	var hotCurrentHash = "077539a1b97487a81fc0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
